5b3c4f83077f635692d075d43ef88d39375f9450
Pedro Perez <pipr96@gmail.com>
2023-08-28 19:46:22 -0700

    clean app

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,10 @@
-import React, { useEffect, useState } from 'react'
-import { Routes, Route } from 'react-router'
+import React, { useState } from 'react'
+import { Routes, Route } from 'react-router'//get routes
 import '../style/index.css'
-//get our fetch functions
-import { getProducts } from '../apiCalls'
-import Navbar from './Navbar'
+import Navbar from './Navbar' //import navbar component
 
 const App = () => {
   const [products, setProducts] = useState([])
-
-  useEffect(() => { 
-    const getData = async ()=>{
-      try {
-        const p = await getProducts();
-        setProducts(p);
-      } catch (error) {
-        console.error;
-      }
-    }
-    getData();
-  }, [])
 
   return (
     <Routes>
