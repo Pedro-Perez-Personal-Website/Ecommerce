8fe552fb3262393779da2a9e57c4556ed8b4bda0
Pedro Perez <pipr96@gmail.com>
2023-08-28 19:42:44 -0700

    Create navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+//Navigation bar
+
+export default function Navbar(){
+
+    return(
+        <nav>
+            <span>menu</span>
+            <label>Search:
+                <input type="text" />
+            </label>
+            <ol>
+                <li>Home</li>
+                <li>Products</li>
+                <li>User</li>
+            </ol>
+            <span>kart</span>
+        </nav>
+    )
+}
