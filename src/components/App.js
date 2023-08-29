42ee1963e7b45c03b047d739ad7da10e95dc288f
Pedro Perez <pipr96@gmail.com>
2023-08-28 19:19:35 -0700

    remove pokemon api

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,31 @@
 import React, { useEffect, useState } from 'react'
 import '../style/index.css'
+//get our fetch functions
+import { getProducts } from '../apiCalls'
 
 const App = () => {
-  const [pokemon, setPokemon] = useState([])
+  const [products, setProducts] = useState([])
 
-  useEffect(() => {
-    const getAllData = async () => {
+  useEffect(() => { 
+    const getData = async ()=>{
       try {
-        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20')
-        const { results } = await response.json()
-        setPokemon(results)
-      } catch (err) {
-        console.error(err)
+        const p = await getProducts();
+        setProducts(p);
+      } catch (error) {
+        console.error;
       }
     }
-    getAllData()
+    getData();
   }, [])
 
   return (
     <div>
       <p>Hello World</p>
-      {!!pokemon.length &&
-        pokemon.map((el, i) => {
+      {!!products.length &&
+        products.map((el, i) => {
           return (
             <div key={i}>
-              <h1>{el.name}</h1>
+              <h1>{el.title}</h1>
             </div>
           )
         })}
