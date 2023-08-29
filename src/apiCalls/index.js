f9cf61d85c8e5d8dc0094ad2983bb45f02e2b829
Pedro Perez <pipr96@gmail.com>
2023-08-28 18:58:13 -0700

    add try/catch blocks

--- src/apiCalls/index.js
+++ src/apiCalls/index.js
@@ -6,56 +6,80 @@ const baseUrl = "https://fakestoreapi.com";
 //1.- Get products!--------------------------------------------
 //get all the products
 async function getProducts(){
-    const response = await fetch(`${baseUrl}/products`);
-    const res = await response.json();
-    //console.log(res);
-    return res;
+    try {
+        const response = await fetch(`${baseUrl}/products`);
+        const res = await response.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.error
+    }
 }
 //getProducts();
 
 //by the category
 async function getCategories(){
-    const response = await fetch(`${baseUrl}/products/categories`);
-    const res = await response.json();
-    console.log(res);
-    return res;
+    try {
+        const response = await fetch(`${baseUrl}/products/categories`);
+        const res = await response.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.error;
+    }
 }
 //getCategories();
 
 //by id
 async function getProductsById(id){
-    const response = await fetch(`${baseUrl}/products/${id}`);
-    const res = await response.json();
-    console.log(res);
-    return res;
+    try {
+        const response = await fetch(`${baseUrl}/products/${id}`);
+        const res = await response.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.error;
+    }
 }
 //getProductsById(2);
 
 //get by specific category
 async function getSpecificCategory(category){
-    const response = await fetch(`${baseUrl}/products/category/${category}`);
-    const res = await response.json();
-    console.log(res);
-    return res;
+    try {
+        const response = await fetch(`${baseUrl}/products/category/${category}`);
+        const res = await response.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.error;
+    }
 }
 //getSpecificCategory('jewelery');
 
 //2.- Get users-------------------------------------------------
 //all users
 async function getUsers(){
-    const response = await fetch(`${baseUrl}/users`);
-    const res = await response.json();
-    //console.log(res);
-    return res;
+    try {
+        const response = await fetch(`${baseUrl}/users`);
+        const res = await response.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.error;
+    }
 }
 //getUsers();
 
 //single user
 const getSingleUser = async (id)=>{
-    const response = await fetch(`${baseUrl}/users/${id}`);
-    const res = await response.json();
-    console.log(res);
-    return res;
+    try {
+        const response = await fetch(`${baseUrl}/users/${id}`);
+        const res = await response.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.error;
+    }
 }
 //getSingleUser(2);
 
@@ -63,18 +87,26 @@ const getSingleUser = async (id)=>{
 //3.- Get kart --------------------------------------------------
 //all products from kart
 const getKart = async ()=>{
-    const response = await fetch(`${baseUrl}/carts`);
-    const res = await response.json();
-    console.log(res);
-    return res;
+    try {
+        const response = await fetch(`${baseUrl}/carts`);
+        const res = await response.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.error;
+    }
 }
 //getKart();
 
 //get user kart
 const getUserKart = async (id)=>{
-    const response = await fetch(`${baseUrl}/carts/user/${id}`);
-    const res = await response.json();
-    console.log(res);
-    return res;
+    try {
+        const response = await fetch(`${baseUrl}/carts/user/${id}`);
+        const res = await response.json();
+        console.log(res);
+        return res;
+    } catch (error) {
+        console.error;
+    }
 }
 //getUserKart(2);
