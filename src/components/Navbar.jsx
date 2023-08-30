5617b03957da2b863ccd36536b6f89abc1e21169
Pedro Perez <pipr96@gmail.com>
2023-08-29 18:38:39 -0700

    add sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,44 @@
 //Navigation bar
+import { getCategories } from "../apiCalls"
+import { useEffect,useState } from "react"
 
-export default function Navbar(){
+//Sidebar
+const Sidebar = ({categories})=>{
+
+    return(
+        <div>
+           <h4>Categories</h4>
+           <ol id='categories'>
+            {categories.map((e)=>{
+                return <li>{e}</li>
+            })}
+           </ol>
+        </div>
+    )
+}
+
+
+
+
+export default function Navbar({setCategories, categories}){
+
+    const [sidebar, setSidebar] = useState();
+    
+    useEffect(()=>{
+        const setData= async()=>{
+            try {
+                const res = await getCategories();
+                setCategories(res);
+            } catch (error) {
+                console.error
+            }
+        }
+        setData();
+    },[])  
 
     return(
         <nav>
-            <span>menu</span>
+            <Sidebar categories={categories}/>
             <label>Search:
                 <input type="text" />
             </label>
