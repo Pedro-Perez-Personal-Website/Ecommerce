0fa2c13bf5e6c841191e129c3651f4cdae5e659b
Pedro Perez <pipr96@gmail.com>
2023-08-31 19:58:41 -0700

    add user page

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import '../style/index.css'
 import Navbar from './Navbar' //import navbar component
 import Productos from '../pages/Productos'
 import Home from '../pages/Home'
+import User from '../pages/User'
 
 const App = () => {
   const [products, setProducts] = useState([]);
@@ -20,6 +21,7 @@ const App = () => {
                                         setProducts={setProducts}
                                         categories={categories}
                                         />}/>
+      <Route path='user' element={<User/>}/>                                  
                                         
       </Route>
     </Routes>
---
+++ src/pages/User.jsx
@@ -0,0 +1,10 @@
+//this is our user page
+
+export default function User(){
+
+    return(
+        <>
+            <h2>User</h2>
+        </>
+    )
+}
