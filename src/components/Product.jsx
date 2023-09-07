0d283769c3ed7451ca8843c38e21737244e38750
Pedro Perez <pipr96@gmail.com>
2023-09-06 18:43:01 -0700

    related content & onclick routing

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -9,11 +9,11 @@ export default function Product(props){
             <img src={props.product.image} alt="" />
             <h3>{props.product.title}</h3>
             <span>{props.product.category}</span>
-            <p>Rating: {props.product.rating.rate}</p>
+            
             <p>$ {props.product.price}</p>
             <p>{props.product.description}</p>
             <button>add kart</button>
-            {//<Products products={props.products} category={props.product.category} key={'1'} />
+            {<Products products={props.products} category={props.product.category} key={'1'} />
         }</section>
         
     )
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,6 @@
 //here we will render our categories
 import Product from "./Product";
+import {useNavigate} from 'react-router-dom'
 
 //Here well render the products image
 const Gallery = ({products, category})=>{
@@ -7,13 +8,15 @@ const Gallery = ({products, category})=>{
     const productos = products.filter((item) => item.category == category);
     console.log(productos);
 
+    let nav = useNavigate();
+
     return(
             <div>
                 {productos.map((e,i) =>{
                 return(
                 
-                    <span key={i}>
-                        <img src={e.image} alt={e.title}/>
+                    <span key={i} onClick={()=>{nav(`/products/${e.id}`)}}>{//when product clicked send to product details
+                    }   <img src={e.image} alt={e.title}/>
                         <p>Nombre de Producto</p>
                         <p>{e.price}</p>
                     </span>
