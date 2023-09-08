bcdc80dbdf15a5b65809061c47ec558b544ecd91
Pedro Perez <pipr96@gmail.com>
2023-09-07 19:34:43 -0700

    clean navbar

--- src/components/Kart.jsx
+++ src/components/Kart.jsx
@@ -1,27 +1,29 @@
 //this is our kart where we will store our quantity and subtotal price
-import {getUserKart} from '../apiCalls/index'
-import { useEffect, useState } from 'react'
 
-//This is our kart component, an icon that stores the # of items
-export default function Kart(props){//props: user,
-    //set our state for the current kart
-    const [kart, setKart] = useState(null);
-
-    //useEffect to fetch the kart data from the api
-    useEffect(()=>{
-        console.log("user:",props.user)
-        const setData = async()=>{
-            const res = await getUserKart(props.user.id);//declare a variable for the response and send the user id to the function
-            console.log('Kart:',res[0]);
-            setKart(res[0]);
-        setData();//invoke function
-        }
-    },[props.user]);//each time we change our user will get a different kart
+//here we have our Kart preview----------------------------------
+const KartDisplay = (props)=>{
+    //we have to decompose kart
+    
 
     return(
-        <button>
-            <span id="counter">n</span>{//n is the number of items store on our kart
-        }   <img src="https://github.com/Pedro-Perez-Personal-Website/Ecommerce/blob/dev-navbar/src/images/kart.png?raw=true" alt="" />
-        </button>
+        <div>
+            
+        </div>
+    )
+}
+
+
+
+//This is our kart component, an icon that stores the # of items-------------------------
+export default function Kart(props){//props: user,
+
+    return(
+        <>
+            <button>
+                <span id="counter">n</span>{//n is the number of items store on our kart
+            }   <img src="https://github.com/Pedro-Perez-Personal-Website/Ecommerce/blob/dev-navbar/src/images/kart.png?raw=true" alt="" />
+            </button>
+            <KartDisplay kart={props.kart}/>
+        </>
     )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 //Navigation bar
-import { getCategories, getProducts } from "../apiCalls"
-import { useEffect,useState } from "react"
-import { Link, Outlet } from "react-router-dom"
+import { useState } from "react"
+import { Link } from "react-router-dom"
 import Kart from "./Kart"
-//Sidebar
+
+
+//Sidebar ------------------------------------------------------------------------
 const Sidebar = ({categories, setCategory})=>{
 
     return(
@@ -18,32 +19,16 @@ const Sidebar = ({categories, setCategory})=>{
     )
 }
 
-
-
-const Header = ({setCategories, categories,user, login, setCategory, setProducts})=>{
+//navbar-----------------------------------------------------------------------------------
+export default function Navbar({setCategories, categories,user, login, setCategory, setProducts, kart}){
 
     const [sidebar, setSidebar] = useState(false);
     const eventHandler =()=>{//open side
         console.log("Clicked");
         setSidebar(!sidebar);
     }
-    
-    useEffect(()=>{
-        const setData= async()=>{
-            try {
-                const res = await getCategories();
-                setCategories(res);
-                const pro = await getProducts();
-                setProducts(pro);
-            } catch (error) {
-                console.error
-            }
-        }
-        setData();
-    },[])  
 
     return(
-        <>
         <nav>
             <button onClick={eventHandler} >
                 <img src='https://github.com/Pedro-Perez-Personal-Website/Ecommerce/blob/dev-navbar/src/images/sidemenu.png?raw=true' alt="" />
@@ -57,29 +42,10 @@ const Header = ({setCategories, categories,user, login, setCategory, setProducts
                 <Link to={'/products'}>Products</Link>
                 {login ? <Link to={`/users/${user.id}`}>User</Link> : <Link to={`/login`}>Login</Link>}
             </ol>
-            <Kart user={user}/>
+            <Kart user={user} kart={kart}/>
         </nav>
-        </>
     )
 }
 
 
-
-export default function Navbar({setCategories, categories,user,login, setCategory, setProducts}){
-
-    return(
-        <>
-            <Header 
-                setCategory={setCategory} 
-                setCategories={setCategories} 
-                categories={categories} 
-                user={user}
-                setProducts={setProducts} 
-                login={login}/>
-            <Outlet/>
-        </>
-    )
-    
-}
-
-export {Header, Sidebar};
+export {Sidebar};
