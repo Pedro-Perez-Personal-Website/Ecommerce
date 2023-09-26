d5bf66311f5a1dc1e00af7e01abd5ce576941151
Pedro Perez <pipr96@gmail.com>
2023-09-25 18:27:55 -0700

    create guest user and kart

---
+++ src/apiCalls/guest.js
@@ -0,0 +1,15 @@
+//this is the guest user
+const guest = {
+    name:"Guest",
+    id:0
+}
+const cart = {
+    id:0,
+    products: [
+        {
+            productId:0,
+            quantity: 0
+        }
+    ]
+}
+export {guest, cart};
