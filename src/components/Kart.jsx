a62eb9762d5c07cb1bb649cef4494afda74cfb82
Pedro Perez <pipr96@gmail.com>
2023-09-25 18:26:26 -0700

    render productId

--- src/components/Kart.jsx
+++ src/components/Kart.jsx
@@ -1,29 +1,45 @@
 //this is our kart where we will store our quantity and subtotal price
+import { useEffect, useReducer } from "react"
+import { getUserKart } from "../apiCalls";
+import Product from "./Product";
+
 
 //here we have our Kart preview----------------------------------
 const KartDisplay = (props)=>{
     //we have to decompose kart
-    
+    console.log("kart",props.kart);
+    const kartList = [];
 
     return(
-        <div>
-            
+        <div id="kart">
+            {props.kart.products.map((e)=>{
+                return(
+                    <p>{e.productId}</p>
+                )
+            })}
         </div>
     )
 }
 
 
 
+
 //This is our kart component, an icon that stores the # of items-------------------------
 export default function Kart(props){//props: user,
-
+    //
+    // props.products.map(e=>{console.log("hello",e.id)});
+    // let p = props.products.find((producto)=>producto.id === 10);
+    // console.log("id:",p);
     return(
         <>
             <button>
                 <span id="counter">n</span>{//n is the number of items store on our kart
             }   <img src="https://github.com/Pedro-Perez-Personal-Website/Ecommerce/blob/dev-navbar/src/images/kart.png?raw=true" alt="" />
             </button>
-            <KartDisplay kart={props.kart}/>
+            {props.user && <KartDisplay kart={props.kart} products={props.products}/>}
         </>
     )
 }
+
+
+
