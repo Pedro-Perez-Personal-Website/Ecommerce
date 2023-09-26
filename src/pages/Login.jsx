82e6a9840b85bbb8557cf4d33ec8841e341dd837
Pedro Perez <pipr96@gmail.com>
2023-09-25 18:19:41 -0700

    set kart

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,29 @@
 //this is our user page
 import { LoginForm,RegisterForm } from "../components/Forms" 
-import { useState} from "react";
+import { useEffect, useState} from "react";
+import { getUserKart } from "../apiCalls";
 
 export default function Login(props){
 
     const [token, setToken]= useState(null);
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    useEffect(()=>{
+        try {
+            const setData = async()=>{
+                if(props.user.id>0){
+                    const data = await getUserKart(props.user.id);
+                    console.log("data:", data);
+                    props.setKart(data[0]);
+                }
+
+            }
+            setData();
+        } catch (error) {
+            console.error
+        }
+    },[props.user]);
 
     return(
         <>
@@ -18,7 +35,10 @@ export default function Login(props){
                         token={token} 
                         setToken={setToken}
                         setUser={props.setUser}
-                        setLogin={props.setLogin}/>                       
+                        user= {props.user}
+                        setLogin={props.setLogin}
+                        kart={props.kart}
+                        setKart={props.setKart}/>                       
             <RegisterForm/>
             
         </>
