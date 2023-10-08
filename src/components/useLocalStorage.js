67a60507b16613adc17c0627a3d8af9c6501ede7
Pedro Perez <pipr96@gmail.com>
2023-10-08 14:31:16 -0700

    created a hook that allows me to store a local value

---
+++ src/components/useLocalStorage.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function useLocalStorage(key, initialValue){
+    const [storedValue, setStoredValue] = useState(()=>{
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            return initialValue;
+        }
+    });
+
+    const setValue = (value)=>{
+        try {
+            setStoredValue(value);
+            window.localStorage.setItem(key, JSON.stringify(value));//why stringify our value because localStorage only accepts strings.
+        } catch (error) {
+            console.error   
+        }
+    }
+    return [storedValue, setValue];
+}
