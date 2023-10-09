4cc1443b3e73cf62f46f25eea2894f2ada20ba1f
Pedro Perez <pipr96@gmail.com>
2023-10-09 07:12:14 -0700

    display kart

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 //Navigation bar
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
+import { Link, Navigate } from "react-router-dom"
 import { getKart, getProductsById, getUserKart } from "../apiCalls";
 import Kart from "./Kart"
 
@@ -9,6 +9,8 @@ export default function Navbar(props){
     //nav states
     const [sidebar, setSidebar] = useState(false);
     const [cart, setCart] = useState(false);
+    const [list, setList] = useState([]);
+    const [titles, setTitles] = useState([]);
 
 
     const eventHandler =()=>{//open side
@@ -31,6 +33,24 @@ export default function Navbar(props){
                 const kart = await getUserKart(localStorage.getItem('localUser'));
                 console.log("cart:", kart[0]);
                 const carrito = kart[0];
+                const productos = carrito.products;
+                let lista = [];
+                let titles = [];
+                productos.map(async (e)=>{
+                let product = await getProductsById(e.productId);
+                console.log("title:",product.price)
+                lista.push(product.price);
+                titles.push(product.title);
+
+                })
+                setList(lista);
+                setTitles(titles);
+                console.log("lista:",lista)
+                let sum = 0;
+                list.map((e)=>{
+                    console.log("precio:", e.price)
+                })
+                console.log("Suma:",sum);
                 props.setKart(carrito);
                 console.log("carrito:", carrito)
                 return carrito;
@@ -59,7 +79,7 @@ export default function Navbar(props){
                 {data ? <Link to={`/users/${props.user.id}`}>{window.localStorage.getItem("username")}</Link> : <Link to={`/login`}>Login</Link>}
             </ol>
             {data && <button onClick={openCart}>kart</button>}
-            {cart && <Cart loggedIn={data} cart={props.kart} true={cart} />}
+            {cart && <Cart loggedIn={data} titles={titles} cart={props.kart} true={cart} products={props.products} lista={list}/>}
             {/* <Kart user={props.user} kart={props.kart} login={props.login} products={props.products}/> */}
         </nav>
     )
@@ -93,13 +113,14 @@ const Cart = (props)=>{
     useEffect(()=>{
         const setData = async ()=>{
             try {
-                console.log("hello world", props.cart);
-                let listOfName = [];
-                const productos = props.cart.products.map(async (e)=>{
-                let product = await getProductsById(e.productId);
-                listOfName.push(product.title);
-                });
-                console.log("list:",listOfName);
+                // console.log("hello world", props.cart);
+                // let listOfName = [];
+                // const productos = props.cart.products.map(async (e)=>{
+                // let product = await getProductsById(e.productId);
+                // listOfName.push(product.title);
+                // });
+                // setProducts(listOfName);
+                // console.log("list:",listOfName);
                 
             } catch (error) {
                 console.error
@@ -107,15 +128,51 @@ const Cart = (props)=>{
         }
         setData();
 
-    },[props.true]);
-    
-    
+    },[]);
 
+    
+    const list = [];
+    const totales = [];
+    let i = -1;
+    let sum = 0;
+
+    
     return(
     <>
         {props.loggedIn && <div id="kart">
                                 <h4>Kart</h4>
-                                
+                                <table>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Quantity</th>
+                                        <th>Price</th>
+                                        
+                                    </tr>
+                                {props.cart.products.map((e)=>{
+                                    console.log("in!", e);
+                                    console.log("producto id:",e.productId);
+                                    console.log("Cantidad:", e.quantity)
+                                    console.log("precio:", props.lista[i])
+                                    console.log("total:", props.lista[i]*e.quantity);
+                                    totales.push(props.lista[i]*e.quantity)
+                                    console.log("titles", props.titles[i])
+                                    console.log("totales", totales)
+                                    
+                                    i++;
+                                    return(
+                                        <tr>
+                                            <td>{props.titles[i]}</td>
+                                            <td>{e.quantity}</td>
+                                            <td>{props.lista[i]}</td>
+                                            
+                                        </tr>
+                                        
+                                    )
+                                    
+                                })}
+
+                                </table>
+                                <Link to={'/checkout'}>checkout</Link>
                             </div>}
     </>  
     )
